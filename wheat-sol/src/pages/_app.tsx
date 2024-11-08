3307d6642b658f7965afaf5c8065b67292b87f3a
que-tech <148863642+que-tech@users.noreply.github.com>
2024-11-08 12:13:25 +0100

    Create _app.tsx

---
+++ wheat-sol/src/pages/_app.tsx
@@ -0,0 +1,19 @@
+import type { AppProps } from 'next/app';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import Layout from '../components/Layout';
+
+const theme = createTheme({
+  // You can customize your theme here
+});
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </ThemeProvider>
+  );
+}
