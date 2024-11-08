98806f8ce154eaf9c9932d38f96a6c07022ec0a2
que-tech <148863642+que-tech@users.noreply.github.com>
2024-11-08 12:20:55 +0100

    Update next.config.mjs

--- wheat-sol/next.config.mjs
+++ wheat-sol/next.config.mjs
@@ -1,7 +1,12 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: ['localhost'],
+  },
+  // Ensure we handle mjs/js files correctly
+  pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
+}
 
 export default nextConfig;
